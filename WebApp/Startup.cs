7ca2ed4dae4b419f39fb340a240047b4bb329112
Quang Nguyễn <adquang199x@gmail.com>
2020-03-22 23:05:41 +0700

    create area admin

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -96,6 +96,10 @@ namespace WebApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
             });
             //dbInitializer.Seed().Wait();
         }
